45d31b1a3c310b66dfa859d0dd96a39ac075783e
Xojiakbar77 <xojiakbarabdumavlonov28@gmail.com>
2023-09-28 16:14:27 +0500

    first data

---
+++ script.js
@@ -0,0 +1,63 @@
+//let a = prompt ('0')
+//console.log(a > 0);
+                            //qoldig 0 teng bolsa  bu juft son
+                            //agar qoooldigi 1ga teng bolsa toq son
+// let a = prompt ('1')
+// console.log(a%2==1);
+
+//let b = prompt('2')
+//console.log(b%2==0);
+
+//let a = prompt('2')
+//let b = prompt('3')
+//console.log(a == b);
+
+//let a = prompt('0')
+//let b = prompt('-2')
+//console.log(a > b);
+
+//let a = prompt('0')
+//let b = prompt('0')
+//let c = prompt('0')
+//console.log(a == 0);
+
+// let a = prompt('10')
+// let b = prompt('8')
+// let c = prompt('6')
+// console.log(10)
+
+
+
+
+
+
+
+
+
+
+// let b = prompt()
+// console.log(b % 2==1);
+ 
+// let a = prompt("a son")
+// let b = prompt("b son")
+// console.log(a>2 && b<=3);
+
+
+//let a = prompt()
+//console.log(a>=100 && a%2==1 && a<1000);
+
+// let a = 20
+// let b = 20
+// let d = 20
+// console.log(a==1 || b==1 || d==1);
+
+let a = 5
+let b = -7
+let d = 7
+console.log(Math.abs(a) == Math.abs(b) || Math.abs(b) == Math.abs(d) || Math.abs(d) == Math.abs(a));
+
+
+
+
+let c = 12
+
